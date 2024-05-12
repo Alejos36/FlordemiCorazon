f85eec9c579399299f611f69caf17213b924a9cb
Alejos36 <tnobmaster@gmail.com>
2024-05-12 16:11:40 -0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+// Función para alternar la clase 'active' en el menú desplegable
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle('active');
+});
+// JavaScript Document
